251bd9541f44b3f0c5d0f53ab3eb5c6078da93ab
Zahid Tokur <zahidtokur@gmail.com>
2022-08-27 23:20:19 +0300

    Add crispy_forms

--- events_project/settings.py
+++ events_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    'core'
+    'core',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
